e2cb5b3b7ac280ecb4e54cd37d504f80e0b1b3c4
yong <tkddbs1644@gmail.com>
2023-11-10 09:55:47 +0900

    7. server fetching data(하지만 request waterfalls 패턴 발생, 성능저하가 일어나므로 다음챕터에서 개선해본다)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,30 @@
-export default function Page() {
+import { Card } from "@/app/ui/dashboard/cards"
+import RevenueChart from "@/app/ui/dashboard/revenue-chart"
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
+import { lusitana } from "@/app/ui/fonts"
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data"
+
+export default async function Page() {
+  // 아래 3개의 요청은 일명 "request waterfalls" 이 되면서 성능저하가 일어나는 코드 패턴이다.
+  // 다음 챕터에서 이를 개선해본다.
+  const revenue = await fetchRevenue() // revenue를 콘솔로그로 찍어보면 클라이언트가 아닌 서버(vscode 콘솔)에 찍힘
+  const latestInvoices = await fetchLatestInvoices()
+  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } =
+    await fetchCardData()
+
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <h1>여기는 /dashboard 페이지 입니다.</h1>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
     </main>
   )
 }
