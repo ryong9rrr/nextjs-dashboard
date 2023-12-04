b11da370bd24b115b4b23f9145dd0651706ebb15
yong <tkddbs1644@gmail.com>
2023-12-05 02:52:57 +0900

    16. Adding Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,23 +1,29 @@
-import Pagination from "@/app/ui/invoices/pagination"
-import Search from "@/app/ui/search"
-import Table from "@/app/ui/invoices/table"
-import { CreateInvoice } from "@/app/ui/invoices/buttons"
-import { lusitana } from "@/app/ui/fonts"
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
-import { Suspense } from "react"
-import { fetchInvoicesPages } from "@/app/lib/data"
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 interface PageProps {
   searchParams?: {
-    query?: string
-    page?: string
-  }
+    query?: string;
+    page?: string;
+  };
 }
 
 export default async function Page({ searchParams }: PageProps) {
-  const query = searchParams?.query || ""
-  const currentPage = Number(searchParams?.page) || 1
-  const totalPages = await fetchInvoicesPages(query)
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -35,5 +41,5 @@ export default async function Page({ searchParams }: PageProps) {
         <Pagination totalPages={totalPages} />
       </div>
     </div>
-  )
+  );
 }
